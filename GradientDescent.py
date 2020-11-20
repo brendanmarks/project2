e37888f1150f3b2ddd7957e47612f3169dd88a16
jamesberrypersonal <james.berry.hameln@gmail.com>
2020-11-20 13:57:07 -0500

    GD class

--- GradientDescent.py
+++ GradientDescent.py
@@ -15,13 +15,29 @@ def softmax(z):
     return np.exp(z) / sum(np.exp(z))
 
 
-# Randomizes the order of the (x,y) instances, and outputs batch_size of them
-def minibatch(x, y, batch_size):
+# Shuffles the (x,y) instances, and outputs a list of minibatch_size sized (x,y) tuples
+def minibatch(x, y, minibatch_size):
     x, y = shuffle(x, y)
-    if not batch_size:
-        return x, y
+    minibatches = []
+    if not minibatch_size:
+        minibatches.append((x, y))
     else:
-        return x[0:batch_size], y[0:batch_size]
+        for i in range(0, x.shape[0], minibatch_size):
+            x_mini = x[i:i+minibatch_size]
+            y_mini = y[i:i+minibatch_size]
+            minibatches.append((x_mini, y_mini))
+    return minibatches
+
+
+# Accuracy function
+def accuracy(y, yh):
+    pred = np.argmax(yh, axis=1)
+    return np.mean(pred == y)
+
+
+# Returns the index of the maximum value in a list
+def argmax(lst):
+    return lst.index(max(lst))
 
 
 class GradientDescent:
@@ -31,82 +47,118 @@ class GradientDescent:
        alphaa - learning rate of the optimizer
        beta1 - momentum hyperparameter
        max_iterations - gradient descent termination condition: maximum times iterated
-       epsilon - gradient descent termination condition: minimum gradient size
-       batch_size - size of the minibatch to use (default value of 0 indicates use full batch)
-       adaptive - if true optimizer uses Adam (Adaptive Moment Estimation) rather than gradient
+       max_no_change - gradient descent termination condition: maximum number of iterations
+                         without the validation error decreasing
+       minibatch_size - size of the minibatch to use (default value of 0 indicates use full batch)
+       cost_fn - optional cost function, if included optimizer will calculate and store the
+                    training and validation cost at each iteration
+       adaptive - if true, optimizer uses Adam (Adaptive Moment Estimation) rather than gradient
                     descent with momentum
        beta2 - 2nd hyperparameter for Adam (if using)
+       epsilon - 3rd hyperparameter for Adam (if using), just to avoid numerical issues
        regularize - determines regularization used (if any): 0 indicates no regularization, 1 or 2
                       indicate L1 or L2 regularization respectively
        lambdaa - regularization coefficient if used
-       keep_weights - indicates whether to store weights at each iteration
     """
     # Constructor
-    def __init__(self, alphaa=0.01, beta1=0.9, max_iterations=1e4, epsilon=1e-8, batch_size=0,
-                 adaptive=False, beta2=0.999, regularize=0, lambdaa=0.1, keep_weights=False):
+    def __init__(self, alphaa=0.01, beta1=0.9, max_iterations=1e4, max_no_change=20, minibatch_size=0,
+                 cost_fn=None, adaptive=False, beta2=0.999, epsilon=1e-8, regularize=0, lambdaa=0.1):
 
         self.alphaa = alphaa
         self.beta1 = beta1
         self.max_iterations = max_iterations
-        self.epsilon = epsilon
-        self.batch_size = batch_size
+        self.max_no_change = max_no_change
+        self.minibatch_size = minibatch_size
+        self.cost_fn = cost_fn
 
         self.adaptive = adaptive
         self.beta2 = beta2
+        self.epsilon = epsilon
 
         self.regularize = regularize
         self.lambdaa = lambdaa
 
-        self.keep_weights = keep_weights
-        if keep_weights:
-            self.weight_history = []
+        self.accuracy_tr = []
+        self.accuracy_val = []
+        self.weight_history = []
+        if self.cost_fn:
+            self.cost_tr = []
+            self.cost_val = []
 
     # Run method - delegates work to one of 2 helper methods, dependent on if Adam is being used or not
-    def run(self, x, y, w):
+    def run(self, x_tr, y_tr, x_val, y_val, w):
         if self.adaptive:
-            return self.adam(x, y, w)
+            return self.adam(x_tr, y_tr, x_val, y_val, w)
         else:
-            return self.momentum(x, y, w)
+            return self.momentum(x_tr, y_tr, x_val, y_val, w)
 
     # Gradient Descent with Momentum
-    def momentum(self, x, y, w):
-        grad = np.inf
+    def momentum(self, x_tr, y_tr, x_val, y_val, w):
         t = 1
+        i = 0
         delta_w = 0
 
-        while np.linalg.norm(grad) > self.epsilon and t < self.max_iterations:
-            x_mini, y_mini = minibatch(x, y, self.batch_size)
-            grad = self.gradient(x_mini, y_mini, w)
+        self.accuracy_tr.append(accuracy(y_tr, softmax(np.dot(x_tr, w))))
+        self.accuracy_val.append(accuracy(y_val, softmax(np.dot(x_val, w))))
 
-            delta_w = (self.beta1 * delta_w) + ((1 - self.beta1) * grad)
-            w -= self.alphaa * delta_w
+        while i < self.max_no_change and t < self.max_iterations:
+            for (x_mini, y_mini) in minibatch(x_tr, y_tr, self.minibatch_size):
+                grad = self.gradient(x_mini, y_mini, w)
+                delta_w = (self.beta1 * delta_w) + ((1 - self.beta1) * grad)
+                w -= self.alphaa * delta_w
 
-            if self.keep_weights:
-                self.weight_history.append(w)
+            self.weight_history.append(w)
+            tr_pred = softmax(np.dot(x_tr, w))
+            pred = softmax(np.dot(x_val, w))
+            self.accuracy_tr.append(accuracy(y_tr, tr_pred))
+            self.accuracy_val.append(accuracy(y_val, pred))
+            if self.cost_fn:
+                self.cost_tr.append(self.cost_fn(y_tr, tr_pred))
+                self.cost_val.append(self.cost_fn(y_val, pred))
+
+            if not self.accuracy_val[-1] < self.accuracy_val[-2]:
+                i += 1
+            else:
+                i = 0
             t += 1
-        return w
+
+        return self.weight_history[argmax(self.accuracy_val)]
 
     # Adaptive Moment Estimation
-    def adam(self, x, y, w):
-        grad = np.inf
+    def adam(self, x_tr, y_tr, x_val, y_val, w):
         t = 1
+        i = 0
         m = 0
         s = 0
 
-        while np.linalg.norm(grad) > self.epsilon and t < self.max_iterations:
-            x_mini, y_mini = minibatch(x, y, self.batch_size)
-            grad = self.gradient(x_mini, y_mini, w)
+        self.accuracy_tr.append(accuracy(y_tr, softmax(np.dot(x_tr, w))))
+        self.accuracy_val.append(accuracy(y_val, softmax(np.dot(x_val, w))))
 
-            m = (self.beta1 * m) + ((1 - self.beta1) * grad)
-            s = (self.beta2 * s) + ((1 - self.beta2) * np.power(grad, 2))
-            mh = m / (1 - np.power(self.beta1, t))
-            sh = s / (1 - np.power(self.beta2, t))
-            w -= self.alphaa * mh * grad / (np.sqrt(sh) + self.epsilon)
+        while i < self.max_no_change and t < self.max_iterations:
+            for (x_mini, y_mini) in minibatch(x_tr, y_tr, self.minibatch_size):
+                grad = self.gradient(x_mini, y_mini, w)
+                m = (self.beta1 * m) + ((1 - self.beta1) * grad)
+                s = (self.beta2 * s) + ((1 - self.beta2) * np.power(grad, 2))
+                mh = m / (1 - np.power(self.beta1, t))
+                sh = s / (1 - np.power(self.beta2, t))
+                w -= self.alphaa * mh * grad / (np.sqrt(sh) + self.epsilon)
 
-            if self.keep_weights:
-                self.weight_history.append(w)
+            self.weight_history.append(w)
+            tr_pred = softmax(np.dot(x_tr, w))
+            pred = softmax(np.dot(x_val, w))
+            self.accuracy_tr.append(accuracy(y_tr, tr_pred))
+            self.accuracy_val.append(accuracy(y_val, pred))
+            if self.cost_fn:
+                self.cost_tr.append(self.cost_fn(y_tr, tr_pred))
+                self.cost_val.append(self.cost_fn(y_val, pred))
+
+            if not self.accuracy_val[-1] < self.accuracy_val[-2]:
+                i += 1
+            else:
+                i = 0
             t += 1
-        return w
+
+        return self.weight_history[argmax(self.accuracy_val)]
 
     # Helper method to calculate gradient (and add regularization penalty if any)
     def gradient(self, x, y, w):
